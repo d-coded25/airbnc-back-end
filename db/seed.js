ce654450b5b0483dbf59b2f00130eae7c6bad6e1
Your Name <you@example.com>
2025-09-08 14:33:22 +0100

    Update seed file to drop and create reviews table

--- db/seed.js
+++ db/seed.js
@@ -6,14 +6,17 @@ const { dropTablesQueries } = queries;
 const { dropPropertyTypes } = dropTablesQueries;
 const { dropUsers } = dropTablesQueries;
 const { dropProperties } = dropTablesQueries;
+const { dropReviews } = dropTablesQueries;
 
 const { createTablesQueries } = queries;
 const { createPropertyTypes } = createTablesQueries;
 const { createUsers } = createTablesQueries;
 const { createProperties } = createTablesQueries;
+const { createReviews } = createTablesQueries;
 
 const dropTables = async function () {
   try {
+    await db.query(dropReviews);
     await db.query(dropProperties);
     await db.query(dropUsers);
     await db.query(dropPropertyTypes);
@@ -28,6 +31,7 @@ const createTables = async function () {
     await db.query(createPropertyTypes);
     await db.query(createUsers);
     await db.query(createProperties);
+    await db.query(createReviews);
     console.log('Resolved: Create Tables!');
   } catch (err) {
     console.log('Rejected: Create Tables:', err.message);
