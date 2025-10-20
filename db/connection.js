5f67b7682a3cf7aeb2dc6a407814b71863957348
Your Name <you@example.com>
2025-10-20 10:38:12 +0100

    Add logic to the connection file to connect to the remote database when the seed production script is run

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,13 @@ const ENV = process.env.NODE_ENV || 'development';
 const dotEnv = require('dotenv');
 dotEnv.config({ path: `${__dirname}/../.env.${ENV}` });
 
-const pool = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const pool = new Pool(config);
 
 module.exports = pool;
